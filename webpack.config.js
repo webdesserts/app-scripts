fb8bc36bf5f5f3ed9e79baa13a78ff829530c75d
Michael Mullins <michael@webdesserts.com>
2019-05-16 13:11:21 -0500

    reformat

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
-const webpack = require('webpack');
-const path = require('path');
-const PrettyWebpack = require('./pretty-webpack-plugin')
-const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default
-const chalk = require('chalk')
+const webpack = require("webpack");
+const path = require("path");
+const PrettyWebpack = require("./pretty-webpack-plugin");
+const createStyledComponentsTransformer = require("typescript-plugin-styled-components")
+  .default;
+const chalk = require("chalk");
 
-const PROJECT_ROOT = path.resolve('./')
+const PROJECT_ROOT = path.resolve("./");
 
 function customErrorFormatter(error, colors) {
-  let position = `${error.line},${error.character}`
-  let code = `TS${error.code}:`
-  return (`${colors.dim(position)} ${colors.bold(code)} ${error.content}`);
+  let position = `${error.line},${error.character}`;
+  let code = `TS${error.code}:`;
+  return `${colors.dim(position)} ${colors.bold(code)} ${error.content}`;
 }
 
 module.exports = {
-  stats: 'none',
-  mode: 'production',
-  devtool: 'source-map',
+  stats: "none",
+  mode: "production",
+  devtool: "source-map",
   output: {
-    path: path.resolve(PROJECT_ROOT, 'app/build/')
+    path: path.resolve(PROJECT_ROOT, "app/build/")
   },
 
   module: {
-    rules: [{
-      test: /\.jsx?|\.tsx?$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-          options: {
-            getCustomTransformers: [createStyledComponentsTransformer()],
-            errorFormatter: customErrorFormatter
+    rules: [
+      {
+        test: /\.jsx?|\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("ts-loader"),
+            options: {
+              getCustomTransformers: [createStyledComponentsTransformer()],
+              errorFormatter: customErrorFormatter
+            }
           }
-       }
         ],
         rules: [
           {
@@ -43,6 +45,6 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  }
 };
